2341861d44a5ad7715cfdb64d9c164e2f3194c00
IvanAgalakov <ivan.se.agalakov@gmail.com>
2023-11-19 01:52:20 -0700

    It is red but everything else works

--- realtimeReader.py
+++ realtimeReader.py
@@ -11,6 +11,7 @@ import pyedflib
 from pyedflib import highlevel
 from datetime import datetime
 import cv2
+from PIL import Image as im
 
 def follow(thefile):
     thefile.seek(0,2)
@@ -60,25 +61,58 @@ def makeImage(window):
     package = [f,t,image]
     images.append(package)
 
+    
     # Create a matplotlib figure for rendering
-    fig, ax = plt.subplots()
-    im = ax.pcolormesh(t, f, image[:, :, 0], shading='gouraud')
-    ax.set_ylim(0, 40)
-
-    # Convert the figure to an image
-    fig.canvas.draw()
-    img = np.array(fig.canvas.renderer.buffer_rgba())
-
+    images = []
+    for i in range(4):
+        fig, ax = plt.subplots()
+        fig.tight_layout()
+        ax.axis("off")
+        im = ax.pcolormesh(t, f, image[:, :, i], shading='gouraud')
+        ax.set_ylim(0, 40)
+        
+        # Convert the figure to an image
+        fig.canvas.draw()
+        img = np.array(fig.canvas.renderer.buffer_rgba())
+        images.append(img)
+        plt.close(fig)
     # Display the image using OpenCV
-    cv2.imshow('Spectrogram', img)
+    # cv2.imshow('Spectrogram', img)
+    # cv2.waitKey(1)  # Add a short delay to allow the image to be displayed
+    model_image = combine_4(images)
+    print(model_image.shape)
+    cv2.imshow('Spectrogram', model_image)
     cv2.waitKey(1)  # Add a short delay to allow the image to be displayed
-
-    plt.close(fig)
 
     # plt.pcolormesh(t, f, image[:,:,0], shading='gouraud')
     # plt.show()
     # plt.ylim(0,40)
 
+def crop_white_border(image):
+    mask = np.all(image >= [250, 250, 250, 255], axis=-1)
+    coords = np.argwhere(~mask)
+    if coords.size == 0:  # If the image is completely white
+        return image
+    x_min, y_min = coords.min(axis=0)
+    x_max, y_max = coords.max(axis=0) + 1
+    cropped = image[x_min:x_max, y_min:y_max]
+    return cropped
+
+
+def combine_4(images):
+    assert(len(images)) == 4
+    img1 = crop_white_border(images[0])
+    img2 = crop_white_border(images[1])
+    img3 = crop_white_border(images[2])
+    img4 = crop_white_border(images[3])
+    
+    images_row_1 = np.concatenate([img1, img2], axis=1)
+    images_row_2 = np.concatenate([img3, img4], axis=1)
+    combined_image = np.concatenate([images_row_1, images_row_2], axis=0)
+    
+    resized_image = cv2.resize(combined_image, (128, 128))
+    
+    return resized_image
 
 
 def main(filepath):
